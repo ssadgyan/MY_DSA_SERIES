57a533897e130f9e99cfb16eb3b891042e6390c8
Sadgyan Singh <dsvvsadgyansinghindo@gmail.com>
2025-11-16 21:58:27 +0530

    Time: 56 ms (38.78%), Space: 57 MB (21.54%) - LeetHub

--- 2789-counter-ii/2789-counter-ii.js
+++ 2789-counter-ii/2789-counter-ii.js
@@ -6,4 +6,21 @@ var createCounter = function(init) {
         decrement: () => --currentCount,
         reset: () => currentCount = init
     };
+};var createCounter = function(init) {
+    let currentCount = init;
+    
+    return {
+        increment: function() {
+            currentCount += 1;
+            return currentCount;
+        },
+        decrement: function() {
+            currentCount -= 1;
+            return currentCount;
+        },
+        reset: function() {
+            currentCount = init;
+            return currentCount;
+        }
+    };
 };
