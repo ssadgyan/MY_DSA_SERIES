0281f0abeef3724144592eeaff439f1701383b97
Sadgyan Singh <dsvvsadgyansinghindo@gmail.com>
2025-12-18 23:23:40 +0530

    Time: 0 ms (100%), Space: 9 MB (42.11%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n <= 0) return false;
-        if(n == 1) return true;
-        
-        while(n % 3 == 0) {
-            n = n/3;
-            if(n == 1) {
-                return true;
-            }
-        }
-        return false;
+        if (n < 0 || n == 0)
+            return false;
+        return (1162261467 % n == 0);
     }
 };
