c70d3c58ec61c3164b9fc944e3325af03f5f9da5
Sadgyan Singh <dsvvsadgyansinghindo@gmail.com>
2025-12-18 23:35:49 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.28%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+
+        if(n <= 0 )  return false;
+        if(n ==  1)return true;
+
+        while(n % 4 == 0){
+            n = n/4;
+            if(n == 1)return true;
+        }
+        return false;
+        
+        
+    }
+};
