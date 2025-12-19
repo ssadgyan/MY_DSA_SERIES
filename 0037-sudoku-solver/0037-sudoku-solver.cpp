18961748fb1a608b5a511fce41ac1172e6b72474
Sadgyan Singh <dsvvsadgyansinghindo@gmail.com>
2025-12-19 13:32:19 +0530

    Time: 227 ms (74.15%), Space: 8.7 MB (87.98%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+
+    bool issafe(vector<vector<char>>& board,int row ,int col ,char digit){
+
+        //For Horizontel
+        for(int j = 0; j < 9 ; j++){
+            if(board[row][j] == digit){
+                return false;
+            }
+        }
+
+        //For Vartical
+        for(int i = 0; i < 9 ; i++){
+            if(board[i][col] == digit){
+                return false;
+            }
+        }
+        //For Grid 3*3
+        int strow = (row/3)*3;
+        int stcol = (col/3)*3;
+
+        for(int i = strow; i<= strow+2; i++){
+            for(int j = stcol; j<= stcol+2; j++){
+                if(board[i][j]==digit){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool Helper(vector<vector<char>>& board,int row,int col){
+        if(row == 9){
+            return true;
+        }
+        int nextrow = row;
+        int nextcol = col+1;
+        if(nextcol == 9){
+            nextrow = row+1;
+            nextcol = 0;
+        }
+        if(board[row][col] != '.'){
+            return Helper(board,nextrow,nextcol);
+        }
+
+        for(char dig='1';dig <='9';dig++){
+            if(issafe(board,row,col,dig)){
+                board[row][col]=dig;
+            
+             if(Helper(board,nextrow,nextcol)){
+                return true;
+             }
+             board[row][col]='.';
+            }
+        }
+        return false;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        Helper(board,0,0);
+    }
+};
