1916e52b373fc1de3f4c63eab0ac15d499c63cd0
Sadgyan Singh <dsvvsadgyansinghindo@gmail.com>
2025-12-20 21:56:39 +0530

    Time: 15 ms (6.01%), Space: 22.5 MB (5.21%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        vector<vector<char>> matrix;
+    
+    for (const string& s : strs) {
+        vector<char> row;
+        for (char c : s) {
+            row.push_back(c);
+        }
+        matrix.push_back(row);
+    }
+    int row = matrix.size();
+    int col = matrix[0].size();
+    int count = 0;
+    for (int i = 0; i < col; i++) {
+     for (int j = 0; j < row-1; j++) {
+         char temp = matrix[j][i];
+        if(matrix[j+1][i] < temp){
+            count++;
+            break;
+        }
+     }
+  }
+  return count;
+    }
+};
