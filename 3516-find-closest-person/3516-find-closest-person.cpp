7b853c17494c4b44af69e6b8c64ad4e5ed558fcb
Sadgyan Singh <dsvvsadgyansinghindo@gmail.com>
2025-12-27 17:21:20 +0530

    Time: 2 ms (3.63%), Space: 8.5 MB (56.61%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int dx = abs(x - z);
+        int dy = abs(y - z);
+
+        if (dx < dy) return 1;
+        else if (dx > dy) return 2;
+        else return 0;
+    }
+};
